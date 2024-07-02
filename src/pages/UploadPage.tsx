21380237fbbf8b7cf5e6a1f3d8b839d692a0a8f2
Omar-Abosea3 <oa615010@gmail.com>
2024-07-03 00:30:38 +0300

    new edits

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -8,10 +8,20 @@ export default function UploadPage() {
     file: Blob;
   }>();
   const [loading, setLoading] = useState(false);
+  interface DiseaseInfo {
+    arabic_name: string;
+    symptoms: string[];
+    pathogen: string;
+    disease: string;
+    plant: string;
+    favorable_conditions: string[];
+    control_methods: string[];
+  }
   const [result, setResult] = useState<{
     plant: string;
-    disease: string;
-    confidence: string;
+    disease_info: DiseaseInfo;
+    confidence: number;
+    predicted_class:string;
   } | null>(null);
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -36,7 +46,7 @@ export default function UploadPage() {
     const category = localStorage.getItem('category')
     try {
       const response = await axios.post(
-        `https://ahmedsaber-plants-leafs.hf.space/predict/${category}`,
+        `https://ahmedsaber-lite-model.hf.space/predict/${category}`,
         formData,
         {
           headers: {
@@ -44,6 +54,8 @@ export default function UploadPage() {
           }
         }
       );
+      console.log(response.data);
+      
       setResult(response.data);
       localStorage.removeItem('category');
     } catch (error) {
@@ -75,19 +87,56 @@ export default function UploadPage() {
         </button>
       </form>
       {result && (
-        <div className="mt-8">
-          <h3 className="text-lg font-semibold mb-2">Detection Result:</h3>
-          <p className="text-gray-700">
-            <span className="text-xl capitalize">plant</span>:{result.plant}
-          </p>
-          <p className="text-gray-700">
-            <span className="text-xl capitalize">disease</span>:{result.disease}
-          </p>
-          <p className="text-gray-700">
-            <span className="text-xl capitalize">confidence</span>:
-            {result.confidence}
-          </p>
+        <div style={{boxShadow:'0px 0px 15px rgba(0, 0, 0,0.5)' , backgroundColor:'rgba(34, 197, 94 , 0.4)' , width:'90%' , margin:'auto' , borderRadius:'18px' , padding:'18px'}}>
+        <div className="px-4 sm:px-0">
+          <h3 className="text-base font-semibold leading-7 text-gray-900">Information About Disease</h3>
+          {/* <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p> */}
         </div>
+        <div className="mt-6 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Plant</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{result.plant}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Disease_Name</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{result.disease_info.arabic_name}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Pathogen</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{result.disease_info.pathogen}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Confidencen</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{result.confidence}</dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Favorable_conditions</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+               {result.disease_info.favorable_conditions.length?result.disease_info.favorable_conditions.map((condition, index) => <span key={index}>
+                <p className="mb-4">{index+1}- {condition}</p>
+               </span>):<p>none</p>}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Control_methods</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+               {result.disease_info.control_methods.length?result.disease_info.control_methods.map((method, index) => <span key={index}>
+                <p className="mb-4">{index+1}- {method}</p>
+               </span>):<p>none</p>}
+              </dd>
+            </div>
+            <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+              <dt className="text-sm font-medium leading-6 text-gray-900">Symptoms</dt>
+              <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+               {result.disease_info.symptoms.length?result.disease_info.symptoms.map((symptom, index) => <span key={index}>
+                <p className="mb-4">{index+1}- {symptom}</p>||<p>no</p>
+               </span>):<p>none</p>}
+              </dd>
+            </div>
+          </dl>
+        </div>
+      </div>
       )}
     </div>
   );
